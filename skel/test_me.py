00db2e0ffc4d57084895d3de918403800fdf27df
Nathan Plamondon <nplamondon@gmail.com>
2022-12-05 18:49:45 -0700

    Clean up day01, update skel tests

--- skel/test_me.py
+++ skel/test_me.py
@@ -2,9 +2,27 @@
 Unit tests
 '''
 
+from skel import *
+
 from . import __main__ as tmain
+
+import pytest
+
+
+@pytest.fixture
+def sample():
+    return []
+
+
+@pytest.fixture
+def parsed():
+    return []
 
 
 def test_main():
     '''Test main'''
     tmain.main()
+
+
+def test_parse_input(sample):
+    assert parse_data(sample) == sample[:-1]
